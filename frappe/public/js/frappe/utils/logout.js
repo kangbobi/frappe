6111a6357f333a82ce98c5cde8bddf0073e3f11f
Kang Bobi <40152293+kangbobi@users.noreply.github.com>
2026-09-01 16:21:11 +0700

    feat: follow OIDC logout redirects

--- frappe/public/js/frappe/utils/logout.js
+++ frappe/public/js/frappe/utils/logout.js
@@ -1,11 +1,28 @@
-frappe.logout = function () {
+frappe.logout = function (fallback) {
 	frappe.call({
 		method: "logout",
 		callback: function (r) {
 			if (r.exc) {
 				return;
 			}
+			if (r.message) {
+				window.location.href = r.message;
+				return;
+			}
+			if (fallback) {
+				fallback();
+				return;
+			}
 			window.location.href = "/login";
 		},
 	});
 };
+
+if (frappe.Application) {
+	frappe.Application.prototype.logout = function () {
+		this.logged_out = true;
+		frappe.confirm(__("Are you sure you want to log out?"), () => {
+			frappe.logout(() => this.redirect_to_login());
+		});
+	};
+}
